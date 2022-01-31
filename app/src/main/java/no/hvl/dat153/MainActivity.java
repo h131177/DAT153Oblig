b987e438139968ec8aa20755fe420dbd915339f2
581257 <54100236+581257@users.noreply.github.com>
2022-01-31 15:28:39 +0100

    MainActivity ferdig ich

--- app/src/main/java/no/hvl/dat153/MainActivity.java
+++ app/src/main/java/no/hvl/dat153/MainActivity.java
@@ -5,20 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
-    protected Map<String, Object> actions = new HashMap<>();
-
-    void prepareMenu() {
-        //adding them to the map
-        addMenuItem("Forms", dbActivity.class);
-        addMenuItem("Indicators", QuizActivity.class);
-        addMenuItem("Containers", AddEntryActivity.class);
-
-    }
+public class MainActivity extends AppCompatActivity  {
 
 
     @Override
@@ -26,14 +24,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        prepareMenu();
-        String[] keys = actions.keySet().toArray(new String[actions.keySet().size()]);
+        //finner knapp med id
+        final Button take_quiz = (Button) findViewById(R.id.take_quiz);
+        //lager intent for å vise hvor det skal sendes videre
+        Intent quiz = new Intent(this,QuizActivity.class);
+        //setter på onClick og sender videre til neste Activity
+        take_quiz.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+               startActivity(quiz);
+            }
+        });
+
+        final Button see_list = (Button) findViewById(R.id.see_list);
+        Intent list = new Intent(this,dbActivity.class);
+        see_list.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(list);
+            }
+        });
+
+        final Button add_entry = (Button) findViewById(R.id.add_entry);
+        Intent entry = new Intent(this,AddEntryActivity.class);
+        add_entry.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(entry);
+            }
+        });
 
 
 
     }
 
-    public void addMenuItem(String label, Class<?> cls) {
-        actions.put(label, new Intent(this, cls));
-    }
+
+
 }
