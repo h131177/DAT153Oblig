58ef4df7c687b36a7186e7d98f9b3e43d670dfdd
h131177 <54102543+h131177@users.noreply.github.com>
2022-01-31 13:37:45 +0100

    Endra navnet på Activity

--- app/src/main/java/no/hvl/dat153/AddEntryActivity.java
+++ app/src/main/java/no/hvl/dat153/AddEntryActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class AddEntry extends AppCompatActivity {
+public class AddEntryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
