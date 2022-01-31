dd31aac61d9a19df21a549f63a1f2f1aafadfc22
581257 <54100236+581257@users.noreply.github.com>
2022-01-31 14:38:05 +0100

    added title size

--- app/src/main/java/no/hvl/dat153/MainActivity.java
+++ app/src/main/java/no/hvl/dat153/MainActivity.java
@@ -2,17 +2,38 @@ package no.hvl.dat153;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.HashMap;
+import java.util.Map;
 
-    //test
+public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
+    protected Map<String, Object> actions = new HashMap<>();
+
+    void prepareMenu() {
+        //adding them to the map
+        addMenuItem("Forms", dbActivity.class);
+        addMenuItem("Indicators", QuizActivity.class);
+        addMenuItem("Containers", AddEntryActivity.class);
+
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Her er ein kommentar
 
-        //Dette er også ein kommentar
+        prepareMenu();
+        String[] keys = actions.keySet().toArray(new String[actions.keySet().size()]);
+
+
+
+    }
+
+    public void addMenuItem(String label, Class<?> cls) {
+        actions.put(label, new Intent(this, cls));
     }
 }
