a98c59e3790d5e829d167c664ab3c878ffa87081
h131177 <54102543+h131177@users.noreply.github.com>
2022-01-31 13:00:53 +0100

    Enda ein kommentar

--- app/src/main/java/no/hvl/dat153/MainActivity.java
+++ app/src/main/java/no/hvl/dat153/MainActivity.java
@@ -12,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Her er ein kommentar
+
+        //Dette er også ein kommentar
     }
 }
