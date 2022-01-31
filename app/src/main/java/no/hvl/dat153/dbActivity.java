fe1fe48c4d406492aef27900ee4da7d9e550ca28
Magnus <31276154+h182036@users.noreply.github.com>
2022-01-31 16:30:20 +0100

    Lagt til RecyclerView i dbActivity

--- app/src/main/java/no/hvl/dat153/dbActivity.java
+++ app/src/main/java/no/hvl/dat153/dbActivity.java
@@ -1,8 +1,16 @@
 package no.hvl.dat153;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import no.hvl.dat153.recyclerview.RecyclerViewFragment;
 
 public class dbActivity extends AppCompatActivity {
 
@@ -10,5 +18,13 @@ public class dbActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_db);
+
+        if (savedInstanceState == null) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            RecyclerViewFragment fragment = new RecyclerViewFragment();
+            transaction.replace(R.id.fragmentContainerView, fragment);
+            transaction.commit();
+        }
     }
+
 }
