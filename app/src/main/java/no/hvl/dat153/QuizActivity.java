758169a518c5a7b3237d56c2c6b8fa87a700cf88
581257 <54100236+581257@users.noreply.github.com>
2022-02-03 14:59:43 +0100

    Lagt til kobling fra quiz til result

--- app/src/main/java/no/hvl/dat153/QuizActivity.java
+++ app/src/main/java/no/hvl/dat153/QuizActivity.java
@@ -2,92 +2,132 @@ package no.hvl.dat153;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
-public class QuizActivity extends AppCompatActivity implements View.OnClickListener{
+public class QuizActivity extends AppCompatActivity implements View.OnClickListener {
 
 
     private String name;
     private String answer;
+    private String displayScore;
     private int score;
     private int total;
     private RadioButton radioButton;
 
+
+    private List<Person> person;
+
+
+    private void mockList() {
+        person = new LinkedList<>(Arrays.asList(new Person("Obama", R.drawable.obama),
+                new Person("Putin", R.drawable.putin),
+                new Person("Trump", R.drawable.trump),
+                new Person("Trump2", R.drawable.trump),
+                new Person("Trump3", R.drawable.trump),
+                new Person("Trump4", R.drawable.trump)));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
-        Random rand = new Random();
-        List<Person> person = new LinkedList<>( Arrays.asList(new Person("Obama", R.drawable.obama),
-                new Person("Beyonce", R.drawable.obama),
-                new Person("Steve Jobs", R.drawable.obama)));
-        int numberOfElements = person.size()-1;
+        mockList();
+        setScreen();
+    }
 
+    private void setScreen() {
+        Random rand = new Random();
+
+        //sender videre til ResultActivity hvis alle navnene er vist
+        if (person.size() == 0) {
+            Intent result = new Intent(this, ResultActivity.class);
+            result.putExtra("score", String.valueOf(score));
+            result.putExtra("total", String.valueOf(total));
+            startActivity(result);
+        }
+
+        //indekser for valgt person og to andre navn, tester litt med de andre for å se hvordan random oppfører seg
+        int randomIndex = rand.nextInt(person.size());
+        int r2 = rand.nextInt(person.size());
+        int r3 = rand.nextInt(person.size());
+        int r4 = rand.nextInt(person.size());
+        int r5 = rand.nextInt(person.size());
+        int r6 = rand.nextInt(person.size());
+        System.out.println(" test " + randomIndex + " " + r2 + " " + r3);
+
+        //henter ut rikitg navn og de andre navn alternativene
+        name = person.get(randomIndex).getName();
+        String r2n = person.get(r2).getName();
+        String r3n = person.get(r3).getName();
+
+        // Henter ut knapper og setter de i en liste for å shuffle rekkefølgen
         Button radioButton1 = (Button) findViewById(R.id.radioButton1);
         Button radioButton2 = (Button) findViewById(R.id.radioButton2);
         Button radioButton3 = (Button) findViewById(R.id.radioButton3);
+        List<Button> list = Arrays.asList(radioButton1, radioButton2, radioButton3);
+        Collections.shuffle(list);
+        System.out.println(list);
+//        list.get(0).setText(name);
+//        list.get(1).setText(r2n);
+//        list.get(2).setText(r3n);
+        radioButton1.setText(name);
+        radioButton2.setText(r2n);
+        radioButton3.setText(r3n);
 
-        Button submit = (Button)findViewById(R.id.submit);
-        RadioGroup radioGroup = (RadioGroup)findViewById(R.id.radio_group);
+        //setter bildet
+        ImageView imageView = findViewById(R.id.profile_picture);
+        imageView.setImageResource(person.get(randomIndex).getPath());
 
-        for (int i = 0; i < numberOfElements; i++) {
-
-            //indekser for valgt person og to andre navn
-            int randomIndex = rand.nextInt(person.size());
-            int r2 = rand.nextInt(person.size());
-            int r3= rand.nextInt(person.size());
-
-            //henter ut rikitg navn og de andre navn alternativene
-            name = person.get(randomIndex).getName();
-            String r2n = person.get(r2).getName();
-            String r3n = person.get(r3).getName();
-
-            // må ha noe som velger random knapp å sitte det på
-            radioButton1.setText(name);
-            radioButton2.setText(r2n);
-            radioButton3.setText(r3n);
-            //tar vekk person fra listen etter den har blitt vist
-            person.remove(randomIndex);
-        }
-
-
+        //tar vekk person fra listen etter den har blitt vist for å ikke repetere person
+        person.remove(randomIndex);
     }
-
 
     @Override
     public void onClick(View view) {
         RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radio_group);
+
         int selectedId = radioGroup.getCheckedRadioButtonId();
-                if (selectedId != -1) {
-                    radioButton = (RadioButton) findViewById(selectedId);
-                    if (radioButton != null) {
-                        total++;
-                        String display = "";
-                        answer = (String) radioButton.getText();
-                        if (answer.equals(name)) {
-                            display = answer + " is correct answer";
-                            score++;
-                        } else {
-                            display = answer + " is wrong answer";
-                        }
-                        Toast.makeText(QuizActivity.this, display, Toast.LENGTH_SHORT).show();
-                    }
+        if (selectedId != -1) {
+            radioButton = (RadioButton) findViewById(selectedId);
+            if (radioButton != null) {
+                total++;
+                String display = "";
+                answer = (String) radioButton.getText();
+                if (answer.equals(name)) {
+                    display = answer + " is correct answer";
+                    score++;
                 } else {
-                    Toast.makeText(QuizActivity.this, "choose one", Toast.LENGTH_SHORT).show();
+                    display = answer + " is wrong answer";
                 }
+                Toast.makeText(QuizActivity.this, display, Toast.LENGTH_SHORT).show();
+                displayScore = "Score: " + score + " of " + total;
+                TextView textView = findViewById(R.id.score);
+                textView.setText(displayScore);
+                radioGroup.clearCheck();
+
+                //setter opp skjermen på nytt
+                setScreen();
+            }
+        } else {
+            Toast.makeText(QuizActivity.this, "choose one", Toast.LENGTH_SHORT).show();
+        }
     }
 }
 
--- app/src/main/java/no/hvl/dat153/ResultActivity.java
+++ app/src/main/java/no/hvl/dat153/ResultActivity.java
@@ -3,12 +3,26 @@ package no.hvl.dat153;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        String score = "";
+        String total = "";
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null){
+            score = extras.getString("score");
+            total = extras.getString("total");
+        }
+        String display = "Your score is " + score + " correct of " + total + " total!";
+        TextView textView = findViewById(R.id.result);
+        textView.setText(display);
     }
 }
