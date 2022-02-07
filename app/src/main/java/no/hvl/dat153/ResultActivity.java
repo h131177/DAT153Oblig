2f6bf1a13f3e500e751d75b749610c734894c8f1
581257 <54100236+581257@users.noreply.github.com>
2022-02-07 17:45:46 +0100

    lagt til flere personer og fikset en tilbakeknapp

--- app/src/main/java/no/hvl/dat153/ResultActivity.java
+++ app/src/main/java/no/hvl/dat153/ResultActivity.java
@@ -12,6 +12,7 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         String score = "";
         String total = "";
