a57aab1e15ebb6438fc155a646b3e72cd6887bd4
h131177 <54102543+h131177@users.noreply.github.com>
2022-03-03 15:13:26 +0100

    Added Room Database

---
+++ app/src/main/java/no/hvl/dat153/AppDatabase.java
@@ -0,0 +1,9 @@
+package no.hvl.dat153;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Person.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract PersonDao personDao();
+}
