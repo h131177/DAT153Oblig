e1e32fbe35a0d6b831a885523430fce5a95fd6cd
581257 <54100236+581257@users.noreply.github.com>
2022-03-03 16:11:03 +0100

    lagt til databasebuilder

--- app/src/main/java/no/hvl/dat153/AppDatabase.java
+++ app/src/main/java/no/hvl/dat153/AppDatabase.java
@@ -1,9 +1,34 @@
 package no.hvl.dat153;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 @Database(entities = {Person.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract PersonDao personDao();
+
+    private static AppDatabase INSTANCE;
+
+//    Room.databaseBuilder(appContext, AppDatabase.class, "Sample.db")
+//            .createFromAsset("database/myapp.db")
+//    .fallbackToDestructiveMigration()
+//    .build();
+
+    static AppDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (AppDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE =
+                            Room.databaseBuilder(context, AppDatabase.class, "person.db")
+                                    .createFromAsset("database/myapp.db")
+                                    .fallbackToDestructiveMigration()
+                                    .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
 }
