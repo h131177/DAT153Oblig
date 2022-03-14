f8f7ef7b49f22392f95ac76b93bda32cb0b633e5
h131177 <54102543+h131177@users.noreply.github.com>
2022-03-14 12:09:40 +0100

    Added a test

---
+++ app/src/androidTest/java/no/hvl/dat153/AddActivityTest.java
@@ -0,0 +1,17 @@
+package no.hvl.dat153;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class AddActivityTest {
+
+    @Rule
+    public ActivityScenarioRule rule = new ActivityScenarioRule<>(AddEntryActivity.class);
+
+    @Test
+    public void addPerson() {
+
+    }
+}
