84628864d6d80a7b5526d09a32fabb643c7e51cc
h131177 <54102543+h131177@users.noreply.github.com>
2022-03-14 12:53:03 +0100

    Test

--- app/src/androidTest/java/no/hvl/dat153/AddActivityTest.java
+++ app/src/androidTest/java/no/hvl/dat153/AddActivityTest.java
@@ -1,5 +1,9 @@
 package no.hvl.dat153;
 
+import android.app.Activity;
+import android.content.Intent;
+
+import androidx.activity.result.ActivityResult;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
 import org.junit.Rule;
@@ -12,6 +16,13 @@ public class AddActivityTest {
 
     @Test
     public void addPerson() {
-
+        // Build the result to return when the activity is launched.
+        Intent resultData = new Intent();
+        String name = "Angela Merkel";
+        String path = "android.resource://no.hvl.dat153/drawable/merkel";
+        resultData.putExtra("name", name);
+        resultData.putExtra("path", path);
+        ActivityResult result =
+                new ActivityResult(Activity.RESULT_OK, resultData);
     }
 }
